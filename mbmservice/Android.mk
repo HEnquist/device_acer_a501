916f722e78191db72cb5ccbd315d31801a505627
Henrik Enquist <henrik.enquist@gmail.com>
2014-04-16 10:36:47 +0200

    Try to add telephony-common

--- mbmservice/Android.mk
+++ mbmservice/Android.mk
@@ -3,6 +3,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_JAVA_LIBRARIES := telephony-common
+
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := MbmService
